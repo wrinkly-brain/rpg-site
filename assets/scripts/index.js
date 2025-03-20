196422cb725a3530ddad2d210c6923d74588c18d
Jonas Mast <jonas.mast@atlasschool.com>
2025-03-20 11:49:53 -0500

    Added basic html to test how to import and bundle files

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,11 @@
-import { Knight, Medic, Performer, Rifleman, Brawler } from './heroes.js';
-import { Attack, Aid } from './abilities.js';
+// import { Knight, Medic, Performer, Rifleman, Brawler } from './heroes.js';
+// import { Attack, Aid } from './abilities.js';
+import { Knight } from './heroes.js';
 
-let party = [Knight, Medic, Performer, Rifleman, Brawler];
 
+const knightButton = document.getElementById("knightButton");
+const knightDiv = document.getElementById("knightDiv");
+
+knightButton.addEventListener("click", () => {
+    knightDiv.innerHTML = '<p>Hp: <p/>' + Knight.hp + '<p>Ap: <p/>' + Knight.ap + '<p>Defense: <p/>' + Knight.defense + '<p>Power: <p/>' + Knight.power + '<p>Speed: <p/>' + Knight.speed + '<p>Abilities: <p/>' + Knight.abilities;
+});
