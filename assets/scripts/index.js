7397ce733ade6994ad41164ccee0d152bc43bdf1
Jonas Mast <jonas.mast@atlasschool.com>
2025-03-31 21:15:21 -0500

    Changed how abilities are displayed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -16,56 +16,56 @@ slimeButton.addEventListener("click", () => {
 });
 
 function displayKnight() {
+    // Display knight stats
     health.innerHTML = '<p>Hp: </p>' + Knight.hp
     ap.innerHTML = '<p>Ap: </p>' + Knight.ap
     defense.innerHTML = '<p>Defense: </p>' + Knight.defense
     speed.innerHTML = '<p>Speed: </p>' + Knight.speed
-    for (let i = 0; i < Knight.abilities.length; i++) {
-        switch (i) {
-            case 0:
-                ability1.innerHTML = '<p>Ability 1: </p>' + Knight.abilities[i].name;
-                break;
 
-            case 1:
-                ability2.innerHTML = '<p>Ability 2: </p>' + Knight.abilities[i].name;
-                break;
+    const abilityContainer = document.getElementById('heroAbilities');
+    abilityContainer.innerHTML = '';
+    
+    // Display all abilities
+    for (let i = 0; i < enemy1.abilities.length; i++) {
+        const ability = enemy1.abilities[i];
 
-            case 2:
-                ability3.innerHTML = '<p>Ability 3: </p>' + Knight.abilities[i].name;
-                break;
+        // Create button
+        const button = document.createElement('button')
+        button.textContent = `${ability.name}`;
+        button.addEventListener('click', () => {
+            // Needs to be another function that prompts player to pick target
+        })
 
-            case 3:
-                ability4.innerHTML = '<p>Ability 4: </p>' + Knight.abilities[i].name;
-                break;
-        }
-    };
+        // Append the button to the ability container
+        abilityContainer.appendChild(button);
+    }
 }
 
 function genEnemy() {
     const enemy1 = createEnemy('slime');
 
+    // Display enemy stats
     enemyName.innerHTML = '<p>Name: </p>' + enemy1.name
     enemyAttribute.innerHTML = '<p>Attribute: </p>' + enemy1.attribute
     enemyHealth.innerHTML = '<p>Hp: </p>' + enemy1.hp
     enemyDefense.innerHTML = '<p>Defense: </p>' + enemy1.defense
     enemySpeed.innerHTML = '<p>Speed: </p>' + enemy1.speed
+
+    const abilityContainer = document.getElementById('enemyAbilities');
+    abilityContainer.innerHTML = '';
+    
+    // Display all abilities
     for (let i = 0; i < enemy1.abilities.length; i++) {
-        switch (i) {
-            case 0:
-                enemyAbility1.innerHTML = '<p>Ability 1: </p>' + enemy1.abilities[i].name;
-                break;
+        const ability = enemy1.abilities[i];
 
-            case 1:
-                enemyAbility2.innerHTML = '<p>Ability 2: </p>' + enemy1.abilities[i].name;
-                break;
+        // Create button
+        const button = document.createElement('button')
+        button.textContent = `${ability.name}`;
+        button.addEventListener('click', () => {
+            // Needs to be another function that prompts player to pick target
+        })
 
-            case 2:
-                enemyAbility3.innerHTML = '<p>Ability 3: </p>' + enemy1.abilities[i].name;
-                break;
-
-            case 3:
-                enemyAbility4.innerHTML = '<p>Ability 4: </p>' + enemy1.abilities[i].name;
-                break;
-        }
+        // Append the button to the ability container
+        abilityContainer.appendChild(button);
     }
 };
