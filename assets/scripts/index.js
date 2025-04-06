7fa3d2f862cc52088268c4243cc339ed88ea8f40
Jonas Mast <jonas.mast@atlasschool.com>
2025-04-06 01:01:59 -0500

    Changed code to test new enemy generation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,71 +1,17 @@
 // import { Knight, Medic, Performer, Rifleman, Brawler } from './heroes.js';
 // import { Attack, Aid } from './abilities.js';
-import { createEnemy } from './enemies.js';
-import { Knight } from './heroes.js';
+import { genRandWave, eraseEnemyParty } from "./enemyManager.js";
 
+const enemyParty = [];
 
-const knightButton = document.getElementById("knightButton");
-const slimeButton = document.getElementById("slimeButton");
+const enemyGenButton = document.getElementById("enemyGenButton");
+const enemyEraseButton = document.getElementById("enemyEraseButton");
 
-knightButton.addEventListener("click", () => {
-    displayKnight();
+enemyGenButton.addEventListener("click", () => {
+    genRandWave(enemyParty);
 });
 
-slimeButton.addEventListener("click", () => {
-    genEnemy();
+enemyEraseButton.addEventListener("click", () => {
+    eraseEnemyParty(enemyParty);
 });
 
-function displayKnight() {
-    // Display knight stats
-    health.innerHTML = '<p>Hp: </p>' + Knight.hp
-    ap.innerHTML = '<p>Ap: </p>' + Knight.ap
-    defense.innerHTML = '<p>Defense: </p>' + Knight.defense
-    speed.innerHTML = '<p>Speed: </p>' + Knight.speed
-
-    const abilityContainer = document.getElementById('heroAbilities');
-    abilityContainer.innerHTML = '';
-    
-    // Display all abilities
-    for (let i = 0; i < Knight.abilities.length; i++) {
-        const ability = Knight.abilities[i];
-
-        // Create button
-        const button = document.createElement('button')
-        button.textContent = `${ability.name}`;
-        button.addEventListener('click', () => {
-            // Needs to be another function that prompts player to pick target
-        })
-
-        // Append the button to the ability container
-        abilityContainer.appendChild(button);
-    }
-}
-
-function genEnemy() {
-    const enemy1 = createEnemy('slime');
-
-    // Display enemy stats
-    enemyName.innerHTML = '<p>Name: </p>' + enemy1.name
-    enemyAttribute.innerHTML = '<p>Attribute: </p>' + enemy1.attribute
-    enemyHealth.innerHTML = '<p>Hp: </p>' + enemy1.hp
-    enemyDefense.innerHTML = '<p>Defense: </p>' + enemy1.defense
-    enemySpeed.innerHTML = '<p>Speed: </p>' + enemy1.speed
-
-    const abilityContainer = document.getElementById('enemyAbilities');
-    abilityContainer.innerHTML = '';
-    
-    // Display all abilities
-    for (let i = 0; i < enemy1.abilities.length; i++) {
-        const ability = enemy1.abilities[i];
-
-        // Create button
-        const button = document.createElement('button')
-        button.textContent = `${ability.name}`;
-        button.addEventListener('click', () => {
-            // Needs to be another function that prompts player to pick target
-        })
-
-        // Append the button to the ability container
-        abilityContainer.appendChild(button);
-    }
-};
