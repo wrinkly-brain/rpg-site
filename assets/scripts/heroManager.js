660f90a0ecbd3e3b49657aeba414b5cb0bcd93b5
Jonas Mast <jonas.mast@atlasschool.com>
2025-04-16 08:57:10 -0500

    Added heroManager and removed some stuff from index

---
+++ assets/scripts/heroManager.js
@@ -0,0 +1,31 @@
+// Gotta add functions to dynamically show and hide hero abilities and stats
+// Add functions to manage hero selection?
+
+// const heroPartyContainer = document.getElementById('heroParty');
+// for (let i = 0; i < heroParty.length; i++) {
+//     const hero = heroParty[i];
+//     const heroDiv = document.createElement('div');
+//     heroDiv.className = 'hero';
+//     heroDiv.dataset.index = i; // Store the index of the hero in the array
+//     heroDiv.innerHTML = `
+//         <h3>${hero.name}</h3>
+//         <p>HP: ${hero.hp}/${hero.maxHp}</p>
+//         <p>AP: ${hero.ap}/${hero.maxAp}</p>
+//         <p>Speed: ${hero.speed}</p>
+//         <p>Defense: ${hero.defense}</p>`;
+// }
+// Maybe I could make the function in the onclick something like: hero.abilities[i].onclick = function() { hero.abilities[i].enableEnemySelection() }
+
+// for (let i = 0; i < Knight.abilities.length; i++) {
+//     const ability = Knight.abilities[i];
+
+//     // Create button
+//     const button = document.createElement('button')
+//     button.textContent = `${ability.name}`;
+//     button.addEventListener('click', () => {
+//         // Needs to be another function that prompts player to pick target
+//     })
+
+//     // Append the button to the ability container
+//     abilityContainer.appendChild(button);
+// }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,41 +13,15 @@ const enemyEraseButton = document.getElementById("enemyEraseButton");
 
 
 addKnightButton.addEventListener("click", () => {
-    const heroPartyContainer = document.getElementById('heroParty');
     const knight = Knight;
     heroParty.push(knight);
 
-    for (let i = 0; i < heroParty.length; i++) {
-        const hero = heroParty[i];
-        const heroDiv = document.createElement('div');
-        heroDiv.className = 'hero';
-        heroDiv.dataset.index = i; // Store the index of the hero in the array
-        heroDiv.innerHTML = `
-            <h3>${hero.name}</h3>
-            <p>HP: ${hero.hp}/${hero.maxHp}</p>
-            <p>AP: ${hero.ap}/${hero.maxAp}</p>
-            <p>Speed: ${hero.speed}</p>
-            <p>Defense: ${hero.defense}</p>
-        `;
 
-        // Maybe I could make the function in the onclick something like: hero.abilities[i].onclick = function() { hero.abilities[i].enableEnemySelection() }
 
-        // for (let i = 0; i < Knight.abilities.length; i++) {
-        //     const ability = Knight.abilities[i];
-    
-        //     // Create button
-        //     const button = document.createElement('button')
-        //     button.textContent = `${ability.name}`;
-        //     button.addEventListener('click', () => {
-        //         // Needs to be another function that prompts player to pick target
-        //     })
-    
-        //     // Append the button to the ability container
-        //     abilityContainer.appendChild(button);
-        // }
+
+
 
         heroPartyContainer.appendChild(heroDiv);
-    }
 });
 
 enemyGenButton.addEventListener("click", () => {
