2d948ffb5b9be9c22ccf52759fdb742695d8ec94
Jonas Mast <jonas.mast@atlasschool.com>
2025-04-25 17:00:37 -0500

    Added function that erases dead enemies from party and a fucntion that updates the enemy display based on dead enemies

--- assets/scripts/enemyManager.js
+++ assets/scripts/enemyManager.js
@@ -24,6 +24,7 @@ function displayEnemyParty(enemyParty) {
         enemyDiv.dataset.index = i; // Store the index of the enemy in the array
         enemyDiv.innerHTML = `
             <h3>${enemy.name}</h3>
+            <p>ID: ${enemy.id}</p>
             <p class="enemy-hp">HP: ${enemy.hp}/${enemy.maxHp}</p>
             <p>Speed: ${enemy.speed}</p>
             <p>Defense: ${enemy.defense}</p>
@@ -43,4 +44,28 @@ function eraseEnemyParty(enemyParty) {
     enemyPartyContainer.innerHTML = ''; // Clear previous enemies
 }
 
-export { genRandWave, eraseEnemyParty };
+// Call function after each turn
+function checkEnemyDeath(enemyParty, target) {
+    if (target.hp <= 0) {
+        const index = target.id
+        enemyParty.splice(index, 1, null) // Dead enemy is replaced with null to keep other enemies in the same positions in array
+    }
+}
+
+function updateEnemyPartyDisplay(enemyParty) {
+    const enemyPartyContainer = document.getElementById('enemyParty');
+
+    // Probably gonna have to make an empty element so that enemy cards stay in position
+    for (let i = 0; i < enemyParty.length; i++) {
+        const enemy = enemyParty[i]
+        const element = document.querySelector(`[data-index="${i}"]`)
+
+        if (enemy == null && element) {
+            element.innerHTML = '' // Clear card for dead enemy
+
+            // element.innerHTML = '<p>Empty Slot</p>'; // Optional: Display "Empty Slot"
+            // element.classList.add('empty'); // Optional: Add a class for styling
+        }
+    }
+}
+export { genRandWave, eraseEnemyParty, checkEnemyDeath, updateEnemyPartyDisplay };
