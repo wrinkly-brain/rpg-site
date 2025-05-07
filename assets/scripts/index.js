7bb55372b7d6f3da5ab14dddf5443df11e3b3b08
Jonas Mast <jonas.mast@atlasschool.com>
2025-05-07 16:44:30 -0500

    Changed knight creation and elements to brawler

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,34 +1,34 @@
 // import { Knight, Medic, Performer, Rifleman, Brawler } from './heroes.js';
 // import { Attack, Aid } from './abilities.js';
-import { Knight } from './heroes.js';
+import { Brawler } from './heroes.js';
 import { genRandWave, eraseEnemyParty } from "./enemyManager.js";
 import { Attack, Aid } from './abilities.js'; // For checking if the ability is an attack or aid
 
 const enemyParty = [];
 const heroParty = [];
 
-const addKnightButton = document.getElementById("addKnightButton");
+const addBrawlerButton = document.getElementById("addBrawlerButton");
 
 const enemyGenButton = document.getElementById("enemyGenButton");
 const enemyEraseButton = document.getElementById("enemyEraseButton");
 
 
-addKnightButton.addEventListener("click", () => {
-    const knight = Knight;
-    const knightDiv = document.getElementById("knightDiv");
-    for (let i = 0; i < knight.abilities.length; i++) {
-        const ability = knight.abilities[i];
+addBrawlerButton.addEventListener("click", () => {
+    const brawler = Brawler;
+    const brawlerDiv = document.getElementById("brawlerDiv");
+    for (let i = 0; i < brawler.abilities.length; i++) {
+        const ability = brawler.abilities[i];
         const button = document.createElement('button');
         button.textContent = `${ability.name}`;
         button.addEventListener('click', () => {
             if (ability instanceof Attack) {
-                ability.enableEnemySelection(knight, enemyParty);
+                ability.enableEnemySelection(brawler, enemyParty);
             }
             if (ability instanceof Aid) {
-                ability.enableAllySelection(knight, heroParty);
+                ability.enableAllySelection(brawler, heroParty);
             }
         });
-        knightDiv.appendChild(button);
+        brawlerDiv.appendChild(button);
     }
 });
 
