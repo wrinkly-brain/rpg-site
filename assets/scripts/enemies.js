6812b5ccabdb7f65f3b6a39b73cf9fba96fcf8bb
Jonas Mast <jonas.mast@atlasschool.com>
2025-05-21 17:02:04 -0500

    Fixed problems with enemy attacks

--- assets/scripts/enemies.js
+++ assets/scripts/enemies.js
@@ -24,10 +24,10 @@ function createEnemy(type, id) {
             speed: 4,
             defense: 1,
             abilities: [
-                new EnemyAttack(1, 'Pointify and Poke', 5, 4),
-                new EnemyAid(1, 'Solidify', 2, 'Defense Up', 2, { isSelfTarget: true }),
-                new EnemyAttack(2, 'Acidic Shot', 3, 3, 'Defense Down', 3),
-                new EnemyAttack(3, 'Slimey Slam', 8, 1, null, 0, { isSingleTarget: false, isAOE: true })
+                new EnemyAttack('Pointify and Poke', 3, 5, null, 0),
+                new EnemyAid('Solidify', 1, 'Defense Up', 2, { isSelfTarget: true }),
+                new EnemyAttack('Acidic Shot', 4, 2, 'Defense Down', 3),
+                new EnemyAttack('Slimey Slam', 8, 1, null, 0, { isSingleTarget: false, isAOE: true })
             ]
         },
 
@@ -40,9 +40,9 @@ function createEnemy(type, id) {
             speed: 7,
             defense: 2,
             abilities: [
-                new EnemyAttack('Shank', 5, 2),
-                new EnemyAttack('Scratch', 4, 4),
-                new EnemyAttack('Throw Rock', 5, 3),
+                new EnemyAttack('Shank', 3, 2, null, 0, { isMultihit: true }),
+                new EnemyAttack('Scratch', 4, 4, null, 0),
+                new EnemyAttack('Throw Rock', 5, 3, null, 0),
                 new EnemyAttack('Feral Rage', 4, 1, 'Bleed', 2)
             ]
         }
