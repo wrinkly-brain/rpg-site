f9552ba49b2750a77b75ce98573f3ccafb2fff01
Jonas Mast <jonas.mast@atlasschool.com>
2025-07-04 16:12:51 -0500

    Replaced enemy controls with battle manager controls

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,69 +1,22 @@
 // import { Knight, Medic, Performer, Rifleman, Brawler } from './heroes.js';
 // import { Attack, Aid } from './abilities.js';
 import { Brawler } from './heroes.js';
-import { genRandWave, eraseEnemyParty } from "./enemyManager.js";
-import { Attack, Aid } from './ability.js'; // For checking if the ability is an attack or aid
-import { chooseRandEnemyAbility, chooseRandTarget, EnemyAttack } from './enemyAbility.js';
-import { updateHeroPartyDisplay } from './heroManager.js';
-import { displayQueue, genTurnQueue } from './turnQueue.js';
+import { BattleManager } from './battleManager.js';
 
 const enemyParty = [];
 const heroParty = [];
 
-const genActiveQueueButton = document.getElementById("genActiveQueueButton");
-
 const addBrawlerButton = document.getElementById("addBrawlerButton");
 
-const enemyGenButton = document.getElementById("enemyGenButton");
-const enemyEraseButton = document.getElementById("enemyEraseButton");
-const enemyAttackButton = document.getElementById("enemyAttackButton");
+const startGameButton = document.getElementById("startGameButton");
 
 
 addBrawlerButton.addEventListener("click", () => {
     const brawler = Brawler;
     heroParty.push(brawler);
-    const brawlerDiv = document.getElementById("brawlerDiv");
-    brawlerDiv.innerHTML = '<h2 class="hero-hp">HP: ' + brawler.hp + '/' + brawler.maxHp + '</h2>';
-    for (let i = 0; i < brawler.abilities.length; i++) {
-        const ability = brawler.abilities[i];
-        const button = document.createElement('button');
-        button.textContent = `${ability.name}`;
-        button.addEventListener('click', () => {
-            if (ability instanceof Attack) {
-                ability.enableEnemySelection(brawler, enemyParty);
-            }
-            if (ability instanceof Aid) {
-                ability.enableAllySelection(brawler, heroParty);
-            }
-        });
-        brawlerDiv.appendChild(button);
-    }
 });
 
-enemyGenButton.addEventListener("click", () => {
-    genRandWave(enemyParty);
+startGameButton.addEventListener("click", () => {
+    const battle = new BattleManager(heroParty, enemyParty);
+    battle.startBattle();
 });
-
-enemyEraseButton.addEventListener("click", () => {
-    eraseEnemyParty(enemyParty);
-});
-
-enemyAttackButton.addEventListener("click", () => {
-    const enemy = enemyParty[0];
-    const ability = chooseRandEnemyAbility(enemy.abilities);
-    console.log(ability);
-    const target = chooseRandTarget(heroParty);
-    if ( ability instanceof EnemyAttack) {
-        ability.applyEnemyAttack(target, heroParty);
-    }
-    else {
-        console.log("It chose an ability")
-    }
-    
-    updateHeroPartyDisplay(heroParty);
-});
-
-genActiveQueueButton.addEventListener("click", () => {
-    const activeQueue = genTurnQueue(heroParty, enemyParty);
-    displayQueue(activeQueue);
-})
