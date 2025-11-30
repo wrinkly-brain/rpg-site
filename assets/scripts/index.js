e755c2f02b11d6f813ce3e5dff1ff70b4c44e1fd
Jonas <jonas.mast@atlasschool.com>
2025-11-30 16:45:26 -0600

    Disable buttons after they're clicked

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,20 +9,28 @@ const heroParty = [];
 
 const addBrawlerButton = document.getElementById("addBrawlerButton");
 const startGameButton = document.getElementById("startGameButton");
-const genHeroDisplayButton = document.getElementById("genHeroDisplayButton")
-
+startGameButton.disabled = true;
 
 addBrawlerButton.addEventListener("click", () => {
     const brawler = Brawler;
     heroParty.push(brawler);
+    addBrawlerButton.disabled = true;
+    if (startGameButton.disabled && heroParty.length > 0) {
+        startGameButton.disabled = false;
+    }
 });
 
 addKnightButton.addEventListener("click", () => {
     const knight = Knight;
     heroParty.push(knight);
+    addKnightButton.disabled = true;
+    if (startGameButton.disabled && heroParty.length > 0) {
+        startGameButton.disabled = false;
+    }
 });
 
 startGameButton.addEventListener("click", () => {
     const battle = new BattleManager(heroParty, enemyParty);
     battle.startBattle();
+    startGameButton.disabled = true;
 });
